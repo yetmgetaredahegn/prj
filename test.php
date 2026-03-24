a6b51c234a4a73bfe055aa295365fc33f5882d79
Yetmgeta Redahegn <yetmgeta.tech@gmail.com>
2026-03-25 01:34:18 +0300

    feat: strings behvior in php

--- test.php
+++ test.php
@@ -1,5 +1,22 @@
 Hello
 
 <?php
-  echo "Hi";
+  $title = 'PHP';
+  $message = "Welcome to $title programming!"; //interpolation works with double quotes
+  echo $message;
+  echo "\n";
+  $m2 = 'This programming language is ' . $title . '!'; //concatenation with single quotes and dot operator
+  echo $m2;
+  //heredoc syntax
+    $heredoc = <<<EOT
+This is a heredoc string. It can span multiple lines and supports variable interpolation like $title.
+EOT;
+  echo "\n";
+  echo $heredoc;
+  //nowdoc syntax
+  $nowdoc = <<<'EOT'
+This is a nowdoc string. It can also span multiple lines but does not support variable interpolation like $title.
+EOT;
+  echo "\n";
+  echo $nowdoc;
 ?>
